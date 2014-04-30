40f7137703491f6a021a150f210a5530b5b7be73
mrthunder <lucasgoes95@gmail.com>
2014-04-30 15:20:32 -0300

    Consertei uns bugs, mas ainda não consigo fazer o texto ficar decente.

--- Project/Project/CadernoDeAtividades.cs
+++ Project/Project/CadernoDeAtividades.cs
@@ -21,6 +21,10 @@ namespace Project
         string[] q1, q2, q3, q4,alt1,alt2,alt3,alt4;
         string Questão1, Questão2, Questão3, Questão4, Questão5, Questão6, Questão7, Questão8, Questão9, Questão10;
         int linhas = 1;
+        int barra1 = 0;
+        int barra2 = 0;
+        int barra3 = 0;
+        int barra4 = 0;
         Color cor1, cor2, cor3, cor4;
         public CadernoDeAtividades(int id, Game1 parent,Episodio01 ep)
             : base(id, parent)
@@ -34,6 +38,10 @@ namespace Project
             if (!initialized)
             {
                 LoadContent();
+                p1 = "";
+                p2 = "";
+                p3 = "";
+                p4 = "";
                 q1 = new string[3] { questions.pergunta00, questions.pergunta40, questions.Pergunta9 };
                 q2 = new string[3] { questions.pergunta10, questions.pergunta6, questions.Pergunta010 };
                 q3 = new string[3] { questions.pergunta20, questions.Pergunta7, "Não tem mais questão." };
@@ -52,70 +60,10 @@ namespace Project
                 alt2 = new string[3] { altQues2, altQues6, altQues10 };
                 alt3 = new string[3] { altQues3, altQues7, " " };
                 alt4 = new string[3] { altQues4, altQues8, " " };
-                for (int i = 0; i < q1[Page].Length; i++)
-                {
-                    if (i % 10*linhas == 0)
-                    {
-                        if (q1[Page][i] == ' ')
-                        {
-                            p1 += "\n";
-                            linhas++;
-                        }
-                    }
-                    
-                    p1 += q1[Page][i];
-                }
-                for (int i = 0; i < q2[Page].Length; i++)
-                {
-                    if (i % 10 * linhas == 0)
-                    {
-                        if (q2[Page][i] == ' ')
-                        {
-                            p2 += "\n";
-                            linhas++;
-                        }
-                    }
-                    if (i == q2[Page].Length - 1)
-                    {
-                        linhas = 0;
-                    }
-                    p2 += q2[Page][i];
-                }
-                for (int i = 0; i < q3[Page].Length; i++)
-                {
-                    if (i % 10 * linhas == 0)
-                    {
-                        if (q3[Page][i] == ' ')
-                        {
-                            p3 += "\n";
-                            linhas++;
-                        }
-                    }
-                    if (i == q3[Page].Length - 1)
-                    {
-                        linhas = 0;
-                    }
-                    p3 += q3[Page][i];
-                }
-                for (int i = 0; i < q4[Page].Length; i++)
-                {
-                    if (i % 10 * linhas == 0)
-                    {
-                        if (q4[Page][i] == ' ')
-                        {
-                            p4 += "\n";
-                            linhas++;
-                        }
-                    }
-                    if (i == q4[Page].Length - 1)
-                    {
-                        linhas = 0;
-                    }
-                    p4 += q4[Page][i];
-                }
+                
                 vlivro = new Vector2(20, 30);
                 vQ1 = new Vector2(vlivro.X + 10, vlivro.Y + 20);
-                vQ2 = new Vector2(vQ1.X, vQ1.Y + 500);
+                vQ2 = new Vector2(vQ1.X, vQ1.Y + 400);
                 vQ3 = new Vector2(vQ1.X + 500, vQ1.Y);
                 vQ4 = new Vector2(vQ3.X, vQ2.Y);
                 
@@ -126,6 +74,104 @@ namespace Project
         Keys lastKey = Keys.A;
         public override void Update(GameTime tempo)
         {
+            if (p1.Length != q1[Page].Length+barra1)
+            {
+                for (int i = 0; i < q1[Page].Length; i++)
+                {
+                    if (i % 10 * linhas == 0)
+                    {
+                        if (q1[Page][i] == ' ')
+                        {
+                            p1 += "\n";
+                            barra1++;
+                            linhas = 0;
+                        }
+                        else
+                        {
+                            linhas++;
+                        }
+                    }
+                    if (i == q1[Page].Length - 1)
+                    {
+                        linhas = 0;
+                    }
+
+                    p1 += q1[Page][i];
+                }
+            }
+            if (p2.Length != q2[Page].Length + barra2)
+            {
+                for (int i = 0; i < q2[Page].Length; i++)
+                {
+                    if (i % 10 * linhas == 0)
+                    {
+                        if (q2[Page][i] == ' ')
+                        {
+                            p2 += "\n";
+                            barra2++;
+
+                        }
+                        else
+                        {
+                            linhas++;
+                        }
+                    }
+                    if (i == q2[Page].Length - 1)
+                    {
+                        linhas = 0;
+                    }
+                    p2 += q2[Page][i];
+                }
+            }
+            if (p3.Length != q3[Page].Length + barra3)
+            {
+                for (int i = 0; i < q3[Page].Length; i++)
+                {
+                    if (i % 10 * linhas == 0)
+                    {
+                        if (q3[Page][i] == ' ')
+                        {
+                            p3 += "\n";
+                            barra3++;
+
+                        }
+                        else
+                        {
+                            linhas++;
+                        }
+                    }
+                    if (i == q3[Page].Length - 1)
+                    {
+                        linhas = 0;
+                    }
+                    p3 += q3[Page][i];
+                }
+            }
+            if (p4.Length != q4[Page].Length + barra4)
+            {
+                for (int i = 0; i < q4[Page].Length; i++)
+                {
+                    if (i % 10 * linhas == 0)
+                    {
+                        if (q4[Page][i] == ' ')
+                        {
+                            p4 += "\n";
+                            barra4++;
+
+                        }
+                        else
+                        {
+                            linhas++;
+                        }
+                    }
+                    if (i == q4[Page].Length - 1)
+                    {
+                        linhas = 0;
+                    }
+                  
+                    p4 += q4[Page][i];
+                }
+            }
             KeyboardState teclado = Keyboard.GetState();
             if (teclado.IsKeyDown(Keys.Back))
             {
@@ -136,6 +182,15 @@ namespace Project
             if ((teclado.IsKeyDown(Keys.Right)) && (lastKey != Keys.Right))
             {
                 Page++;
+                p1 = "";
+                p2 = "";
+                p3 = "";
+                p4 = "";
+                barra1 = 0;
+                barra2 = 0;
+                barra3 = 0;
+                barra4 = 0;
+                
                 if (Page >= 2)
                 {
                     Page = 2;
@@ -145,6 +200,14 @@ namespace Project
             if ((teclado.IsKeyDown(Keys.Left)) && (lastKey != Keys.Left))
             {
                 Page--;
+                p1 = "";
+                p2 = "";
+                p3 = "";
+                p4 = "";
+                barra1 = 0;
+                barra2 = 0;
+                barra3 = 0;
+                barra4 = 0;
                 if (Page <= 0)
                 {
                     Page = 0;
@@ -254,7 +317,7 @@ namespace Project
             SpriteBatch.DrawString(arial, Page.ToString() + "/2", new Vector2(900, 700), Color.Black);
             if (cor1 == Color.Black)
             {
-                SpriteBatch.DrawString(arial, q1[Page] + alt1[Page], vQ1, cor1);
+                SpriteBatch.DrawString(arial, p1 + alt1[Page], vQ1, cor1);
             }
             else
             {
@@ -262,7 +325,7 @@ namespace Project
             }
             if (cor2 == Color.Black)
             {
-                SpriteBatch.DrawString(arial, q2[Page] + alt2[Page], vQ2, cor2);
+                SpriteBatch.DrawString(arial, p2 + alt2[Page], vQ2, cor2);
             }
             else
             {
@@ -270,7 +333,7 @@ namespace Project
             }
             if (cor3 == Color.Black)
             {
-                SpriteBatch.DrawString(arial, q3[Page] + alt3[Page], vQ3, cor3);
+                SpriteBatch.DrawString(arial, p3 + alt3[Page], vQ3, cor3);
             }
             else
             {
@@ -278,7 +341,7 @@ namespace Project
             }
             if (cor4 == Color.Black)
             {
-                SpriteBatch.DrawString(arial, q4[Page] + alt4[Page], vQ4, cor4);
+                SpriteBatch.DrawString(arial, p4 + alt4[Page], vQ4, cor4);
             }
             else
             {
