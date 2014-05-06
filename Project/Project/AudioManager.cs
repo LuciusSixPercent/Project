7fe4cb57879d59c7337842a7fef6900d77304d49
LuciusSixPercent <vlucki@hmail.com>
2014-05-05 23:08:04 -0300

    - Classe AudioManager que faltava;

---
+++ Project/Project/AudioManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Project
+{
+    public class AudioManager
+    {
+        private static AudioEngine audioEngine;
+
+        public static AudioEngine AudioEngine
+        {
+            get 
+            {
+                if (audioEngine == null)
+                {
+                    audioEngine = new AudioEngine("Content\\Audio\\MyGameAudio2.xgs");
+                }
+                return AudioManager.audioEngine; 
+            }
+        }
+        private static WaveBank waveBank;
+
+        public static WaveBank WaveBank
+        {
+            get 
+            {
+                if (waveBank == null)
+                {
+                    waveBank = new WaveBank(AudioEngine, "Content\\Audio\\Wave Bank2.xwb");
+                }
+                return AudioManager.waveBank; 
+            }
+        }
+        private static SoundBank soundBank;
+
+        public static SoundBank SoundBank
+        {
+            get 
+            {
+                if (soundBank == null)
+                {
+                    WaveBank wb = WaveBank;
+                    soundBank = new SoundBank(AudioEngine, "Content\\Audio\\Sound Bank2.xsb");
+                }
+                return AudioManager.soundBank; 
+            }
+        }
+
+
+        public static Cue GetCue(string name)
+        {
+            return SoundBank.GetCue(name);
+        }
+    }
+}
